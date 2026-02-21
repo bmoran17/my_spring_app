17890fec55309ff280d06522811530b722612b1f
Brenda Moran <110500766+bmoran17@users.noreply.github.com>
2026-02-20 20:40:57 -0800

    Created multiple implementations of interface to use @Qualifier to specify which one to use

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/example/spring_core_demo/rest/DemoController.java
@@ -0,0 +1,28 @@
+package com.example.spring_core_demo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.spring_core_demo.common.Coach;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class DemoController {
+
+  // define a private field for the dependency
+  private Coach myCoach;
+
+  // constructor injection
+  @Autowired
+  public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    myCoach = theCoach;
+  }
+
+  // expose a new endpoint for daily workout
+  @GetMapping("/dailyworkout")
+  public String getDailyWorkout() {
+    return myCoach.getDailyWorkout();
+  }
+  
+}
